f83868c08980b1b73840efca4eae46fe27176263
Benjamin Juarez <ben95juarez@gmail.com>
2020-05-28 16:51:25 -0500

    changed email service

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.post('/api/sendMessage', (req, res) => {
     console.log(data);
 
     const transporter = nodemailer.createTransport({
-      service: 'gmail',
+      service: 'yahoo',
       secure: false,
       auth: {
         user: process.env.email,
