a3334baead3a091c8596bff71e3d08841ef72b1b
Moises Nunez <moises5610@gmail.com>
2020-05-28 15:04:07 -0500

    cors moved

--- index.js
+++ index.js
@@ -12,12 +12,11 @@ if (result.error) {
 }
 
 const app = express();
+app.use(cors());
 const port = process.env.PORT;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(cors());
 
 app.listen(port, () => {
   console.log(`Your port is ${process.env.PORT}`);
